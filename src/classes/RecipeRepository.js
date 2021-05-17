e54fb5e4496e76cc2e330078780764cb41448140
Shauna Myers <shaunamyers26@gmail.com>
2021-05-17 14:53:08 -0700

    Add retriveRecipesByTag test

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,9 +3,11 @@ class RecipeRepository {
     this.recipesData = recipesData;
   }
   retrieveRecipesByTag(tag) {
-
+    // Filter through this.recipesData
+    // this.recipesData.tags includes tag
+    this.recipesData.filter(recipe => recipe.tags.includes(tag));
   }
-  retrieveByRecipesOrIngredient() {
+  retrieveRecipesByNameOrIngredient() {
 
   }
 }
