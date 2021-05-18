664c981eaa4b848716a2946b87c653154bda4c52
Shauna Myers <shaunamyers26@gmail.com>
2021-05-18 14:58:44 -0700

    Add ability to retrieve recipes by name or ingredient

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -2,12 +2,13 @@ class Recipe {
   constructor(recipe, ingredientsData) {
     this.id =  recipe.id;
     this.image = recipe.image;
-    this.ingredients = recipe.ingredients;
+    this.ingredients = recipe.ingredients; // an array of objects
     this.instructions = recipe.instructions;
     this.tags = recipe.tags;
     this.ingredientsData = ingredientsData;
-
   }
+
+
 }
 
 export default Recipe;
--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,19 +1,45 @@
+
+
 class RecipeRepository {
   constructor(recipesData) {
     this.recipesData = recipesData;
+    // sampleRecipesData is an array with 6 propertys
+      // sampleRecipesData.ingredients is an array of objects
+      // each object has 2 key value pairs: id & quantity
   }
   retrieveRecipesByTag(tag) {
     tag = tag.toLowerCase();
     let results = this.recipesData.filter(recipe => recipe.tags.includes(tag));
     return results
   }
-  retrieveRecipesByNameOrIngredient(keyword) {
 
+  retrieveRecipesByNameOrIngredient(keyword, ingredientsData) {
+
+    const lowerCaseKeyword = keyword.toLowerCase();
+
+    const matchingRecipes = [];
+
+    const foundIngredient = ingredientsData.filter(ingredient => ingredient.name.includes(lowerCaseKeyword)).map(ingredient => ingredient.id); // an array of ids
+
+    this.recipesData.forEach(recipe => {
+      if (recipe.name.toLowerCase().includes(lowerCaseKeyword)) {
+        matchingRecipes.push(recipe);
+      }
+      recipe.ingredients.forEach(ingredient => {
+        foundIngredient.forEach(id => {
+          if (id === ingredient.id) {
+            matchingRecipes.push(recipe);
+          }
+        })
+      })
+    });
+    console.log('anyone home?', matchingRecipes);
+    return matchingRecipes;
   }
 
-  createNameProperty(recipe, ingredientsData) {
-    //psedo
-  }
+  // createNameProperty(recipe, ingredientsData) {
+  //
+  // }
 }
 
 export default RecipeRepository;
