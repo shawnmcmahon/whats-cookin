933ea285ee9b05260d7f9465e1521041466cf168
Shauna Myers <shaunamyers26@gmail.com>
2021-05-18 18:28:15 -0700

    Add addToRecipesToCook method

--- src/classes/User.js
+++ src/classes/User.js
@@ -16,6 +16,17 @@ class User {
     this.favoriteRecipes.splice(index, 1);
   }
 
+  // Decide to cook a recipe that week (add to my recipesToCook)
+
+  addToRecipesToCook(recipe) {
+    this.recipesToCook.push(recipe);
+  }
+
+// Filter my favoriteRecipes by one or more tags.
+
+  
+// Filter my favoriteRecipes by its name or ingredients.
+
 }
 
 
