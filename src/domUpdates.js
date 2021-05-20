66703bf53424e2c6bd5866b99c42f9c16b0d8faa
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-05-20 14:23:13 -0600

    Begin onStartup function

---
+++ src/domUpdates.js
@@ -0,0 +1,8 @@
+import Recipe from './classes/domUpdates'
+
+let domUpdates = {
+
+}
+
+
+export default domUpdates;
--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,23 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import Recipe from './classes/Recipe'
+import RecipeRepository from './classes/RecipeRepository'
+import Users from './classes/User'
+
+let recipeRepository;
+let globalIngredientsData = {}
+
+function onStartUp() {
+  apiCalls.getData()
+    .then(([userData, ingredientsData, recipeData]) => {
+      user = new User(userData[(Math.floor(Math.random() * userData.length))]);
+      globalIngredientsData = ingredientsData;
+      recipeRepository = new RecipeRepository(recipeData)
+      //domUpdates function that populates all recipe cards to the home page
+
+      //domUpdates function that will greet the user by updating the headline
+    })
+}
 
 
 
